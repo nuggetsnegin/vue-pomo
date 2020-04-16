d4f683b3026d7f402c5c45ca8c3fff724f5b2ab5
Negin <nuggetnegin@gmail.com>
2020-04-16 13:50:11 -0400

    vue setup

--- App.vue
+++ App.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Hello!</h1>
+  <div id="app">
+    <h1>Pomomomo</h1>
+  </div>
 </template>
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.18/vue.min.js"></script>
+
+<script>
+new Vue({
+  el: "#app",
+  data: {}
+});
+</script>
